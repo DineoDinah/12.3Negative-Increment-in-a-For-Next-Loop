a82d2734745da1fe11fd982a5498918ec8c01ac6
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-05 22:11:48 +0200

    Add project files.

---
+++ 12.3Negative Increment in a For Next Loop/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Dim counter, sum As Integer
+		sum = 1000
+		For counter = 100 To 5 Step -5
+			sum -= counter
+			ListBox1.Items.Add(sum)
+		Next
+	End Sub
+End Class
